8f8e6a0549fdd22489ee7451634bce78cbd9d6ba
saif64s <my@wo.rk>
2025-12-13 11:39:56 +0600

    subscription pricing change

--- src/app/modules/payment/payment.constant.ts
+++ src/app/modules/payment/payment.constant.ts
@@ -14,9 +14,9 @@ export const subscriptionPlansDetails: Record<
     string,
     { durationInMonths: number; price: number }
 > = {
-    '1 month': { durationInMonths: 1, price: 200 },
-    '6 months': { durationInMonths: 6, price: 1000 },
-    '1 year': { durationInMonths: 12, price: 1800 },
+    '1 month': { durationInMonths: 1, price: 10 },
+    '6 months': { durationInMonths: 6, price: 30 },
+    '1 year': { durationInMonths: 12, price: 50 },
 };
 
 //filter
